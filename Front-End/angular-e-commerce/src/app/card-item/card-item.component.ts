cd018b80c608451f5dc88b8acc79d7226d959799
khoa pham <khoa.pham@tpssoft.com>
2022-10-24 18:00:18 +0700

    update front-end version 2.6 (update component flash-sale in page index)

--- Front-End/angular-e-commerce/src/app/card-item/card-item.component.ts
+++ Front-End/angular-e-commerce/src/app/card-item/card-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-item.component.scss']
 })
 export class CardItemComponent implements OnInit {
+  @Input() product: string = "";
 
   constructor() { }
 
--- Front-End/angular-e-commerce/src/app/menu-index-page/menu-flash-sale/menu-flash-sale.component.ts
+++ Front-End/angular-e-commerce/src/app/menu-index-page/menu-flash-sale/menu-flash-sale.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-flash-sale.component.scss']
 })
 export class MenuFlashSaleComponent implements OnInit {
+  arrDemo: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.arrDemo = [
+      'Komi - Nữ Thần Sợ Giao Tiếp - Tập 14 - Tặng Kèm Photostrip',
+      '666 Satan - Tập 1 - Tặng Kèm Bookmark Nhân Vật',
+      'Vẽ Vời Vẩn Vơ - Tập 5',
+      'Kaguya-Sama: Cuộc Chiến Tỏ Tình - Tập 3 - Tặng Kèm 1 Trong 2 Mẫu Thẻ Học Sinh',
+      '666 Satan - Tập 1 - Tặng Kèm Bookmark Nhân Vật',
+      'Kinh Doanh - Khái Lược Những Tư Tưởng Lớn'
+    ]
   }
 
 }
